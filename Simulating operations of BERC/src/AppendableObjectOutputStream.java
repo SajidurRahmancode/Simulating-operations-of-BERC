db37006fa74f10250dd12a0f353e0544a6bf2d70
SajidurRahmancode <67187135+SajidurRahmancode@users.noreply.github.com>
2024-03-11 22:25:55 +0600

    update

--- Simulating operations of BERC/src/AppendableObjectOutputStream.java
+++ Simulating operations of BERC/src/AppendableObjectOutputStream.java
@@ -1,9 +1,18 @@
 
-
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
 class AppendableObjectOutputStream extends ObjectOutputStream {
     public AppendableObjectOutputStream(OutputStream out) throws IOException {
         super(out);
